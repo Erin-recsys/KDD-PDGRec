0a01212727a816b653b7b02519a2babbe8dfc022
zqxwcevrtyui <134911845+zqxwcevrtyui@users.noreply.github.com>
2025-02-16 23:20:09 +0800

    Update and rename Get_contrast.py to Get_denoise_graph.py

--- PDGRec/utils/Get_denoise_graph.py
+++ PDGRec/utils/Get_denoise_graph.py
@@ -1,7 +1,7 @@
 import torch
 import os
 
-device = torch.device('cuda:2' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print(f"Using device: {device}")
 
 from utils.dataloader_steam import Dataloader_steam_filtered
@@ -10,7 +10,7 @@ from utils.parser import parse_args
 args = parse_args()
 
 
-path = "/PDGRec/steam_data"
+path = "./steam_data"
 
 
 user_id_path = path + '/users.txt'
@@ -29,14 +29,3 @@ dataloader = Dataloader_steam_filtered(args,
 
 print("Getting denoised graph...")
 denoised_graph = dataloader.Get_Contrast_views(dataloader.graph)
-
-a,b,c=dataloader.calculate_user_genre_noise_and_weights(dataloader.graph,denoised_graph,dataloader.genre_mapping,device)
-print(b)
-output_path = "/PDGRec/user_genre_noise_readable.txt"
-
-with open(output_path, 'w') as f:
-    for user_id, genre_stats in a.items():
-        
-        genre_info = [f"{genre}:{ratio:.3f}" for genre, ratio in genre_stats.items()]
-        line = f"User {user_id}: " + ", ".join(genre_info)
-        f.write(line + '\n')
